5cda8a5c9594308811bc2184605d4857dd919060
Jo <last2599@gmail.com>
2021-09-24 06:20:08 +0900

    Explain Test Structure

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,16 @@
+// Test Structure
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+// Tests Block
 describe("HelloWorld.vue", () => {
+  // Test Block
   it("renders props.msg when passed", () => {
     const msg = "new message";
+    // Test Component
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
-  });
-});
+  }); // Test Block end
+}); // Tests Block end
